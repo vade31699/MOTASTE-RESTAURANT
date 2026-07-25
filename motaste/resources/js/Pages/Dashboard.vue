bf9179a0ebd1b62d91beab2f0a882f97632610c2
vadevidad31699-sketch <vadevidad31699@gmail.com>
2026-07-25 21:42:31 +0800

    update example deploy auto

--- motaste/resources/js/Pages/Dashboard.vue
+++ motaste/resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ import { Head } from '@inertiajs/vue3';
                     class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
                 >
                     <div class="p-6 text-gray-900">
-                        You're logged in!
+                        You're logged in dave!
                     </div>
                 </div>
             </div>
